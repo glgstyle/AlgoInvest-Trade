15315843153d9c3a9f5d2dd691e4754aafcbac6d
glgstyle <gleguenno@gmail.com>
2022-09-22 09:16:02 +0200

    Adding a timer to count the time to execute the function bruteforce

--- controllers/bruteforce.py
+++ controllers/bruteforce.py
@@ -1,9 +1,10 @@
 '''The brute brute force controller'''
-import sys
+import time
 from models.action import Action
 import csv
 liste=[ ('a',10),('b',20),('c',10),('d',40)]
 actions = [1,2,3,4, 5, 6]
+
 class BruteForce:
     def __init__(self):
         '''Has a list of actions and a view'''
@@ -22,72 +23,6 @@ class BruteForce:
             # print(self.actions)
             return self.actions
 
-
-    # def bruteForce(self,actions, selection=[]):
-    #     # actionsList= []
-    #     # for action in actions:
-    #     #     actionsList.append((action.name, action.cost))
-    #     # # print(actionsList)
-    #     # if len(actions) > 0:
-    #     #     first = actionsList.pop(0)
-    #     #     print(actionsList)
-    #     #     print(first)
-
-    #     # costs= []
-    #     # for action in actions:
-    #     #     costs.append(action)
-    #     # print(costs)
-    #     # print(actions)
-    #     actionsCopy = actions
-    #     if len(actions) > 0:
-    #         print(actions)
-    #         first = actions.pop(0)
-    #         # print(first)
-    #         # print(actions.copy())
-    #         # # print(first)
-    #         for i in actions:
-    #             # result1 = self.bruteForce(actions.copy(), selection + [first])
-    #             result1 = self.bruteForce(actionsCopy, selection + [first])
-    #             print(result1)
-            # print(actions)
-            # result1 = self.bruteForce(actions.copy(), selection + [first])
-            # print(result1)
-
-            # while result1 != None:
-            #     print(result1)
-            #     return result1
-
-            # print(len(actions))
-            # if len(actions) == 0 and len(selection == 0):
-            #     return result1
-            
-            # result2 = self.bruteForce(actions.copy(), selection)
-            # if result1 !=None and result1 != result2:
-            #     print("result1", result1)
-            #     print("result2", result2)
-            #     if result1[0] == result2[0]:
-            #         return result1
-            #     else:
-            #         return result2
-        # else:
-        #     cost = 0
-        #     print(selection)
-        #     for s in selection:
-        #         cost = cost + s[1]
-        #     return cost, selection
-
-    # def bruteForce(actions, selection=[]):
-
-
-    # def bruteForce(self, actions, selection=[]):
-    #     for action in actions:
-    #         selection.append(action)
-    #         print(','.join(map(str,selection)))
-    #         if len(selection) == len(actions):
-    #             if len(actions) > 1:
-    #                 popNumber = actions.pop(1)
-    #                 selection=[]
-    #                 self.bruteForce(actions,selection)
     def getActionCostList(self):
         actionsList = self.recordActions()
         actions = []
@@ -125,15 +60,17 @@ class BruteForce:
                 return None
                     
 controller = BruteForce()
-# appeler le start timer
+# start timer
+start = time.time()
+# print("start : ",start)
 # controller.bruteForce(controller.recordActions())
 resultat = controller.bruteForce(controller.getActionCostList())
-# appeler la fonction stop timer
+# stop timer
+end = time.time()
+print("Temps d'éxécution :", end - start, "secondes")
 print("resultat :", resultat)
 
 # controller.bruteForce(actions)
-
-
 # print(controller.actions)
 # print (controller.combinaison(liste))
 
@@ -143,7 +80,5 @@ print("resultat :", resultat)
 #     mettre le résultat dans un tableau ordonné du meilleur au moins bon
 
 # Créer un fichier csv pour importer les données que python doit lire et transformer en objet
- 
 
-#  calculer le temps avant et après l'execution de bruteforce en ajoutant une librairie 
 # conditionner les 500euros le plus tot possible
